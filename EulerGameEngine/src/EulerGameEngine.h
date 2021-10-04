1e09b54cbf19f6b5104cf5af3b082ae412b18c0c
Jenka <jens.johansson94@gmail.com>
2021-10-04 15:35:20 +0200

    Added log functionalities for core and client

--- EulerGameEngine/src/EulerGameEngine.h
+++ EulerGameEngine/src/EulerGameEngine.h
@@ -3,6 +3,7 @@
 // For use by Euler Game Engine application
 
 #include "EulerGameEngine/Application.h"
+#include "EulerGameEngine/Log.h"
 
 // ---Entry Point---------------
 #include "EulerGameEngine/EntryPoint.h"
--- EulerGameEngine/src/EulerGameEngine/EntryPoint.h
+++ EulerGameEngine/src/EulerGameEngine/EntryPoint.h
@@ -1,11 +1,17 @@
 #pragma once
 
+// Can be removed?
 #include"Application.h"
 #ifdef  ELR_PLATFORM_WINDOWS
 extern EulerEngine::Application* EulerEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	EulerEngine::Log::Init();
+	ELR_CORE_WARN("Initialized log!");
+	int a = 5;
+	ELR_INFO("Hello! Var={0}", a);
+
 	auto app = EulerEngine::CreateApplication();
 	app->Run();
 	delete app;
